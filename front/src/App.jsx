a57e8535c9f4ca63f33e6ea8d7047a089ff91756
vugarhasan1 <vugar.hasanov@cybernet.az>
2023-06-08 19:21:04 +0400

    commited

---
+++ front/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h3>Hello minimogo</h3>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus, est.</p>
+    </div>
+  );
+}
+
+export default App;
+
+
